06f29423426ffc1f9bd0015bbc6efa757b9fd4a7
hyejoy <devchoi97@gmail.com>
2026-03-24 13:36:08 +0900

    feat: register 반환하는 객체의 내부 구조 살펴보기 - { name, ref, onChange, onBlur }

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import CleanValidationForm from './react-hook-form/after/CleanValidationForm';
+import RegisterDeepDive from './react-hook-form/RegisterDeepDive';
 import TypeForm from './react-hook-form/TypeForm';
 
 export default function App() {
   return (
     <>
-      <CleanValidationForm />
+      <RegisterDeepDive />
     </>
   );
 }
